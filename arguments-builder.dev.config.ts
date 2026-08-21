e40ca2ba2340f7a8702820d641dc7488b69b73fa
脆薯饼 <Clyne@nanocat.me>
2026-08-21 18:37:21 +0800

    fix(dev): 统一 MapKit 开发版产物名称

--- arguments-builder.dev.config.ts
+++ arguments-builder.dev.config.ts
@@ -4,25 +4,25 @@ import { argsFull } from "./arguments-builder.full.config";
 export default defineConfig({
 	output: {
 		surge: {
-			path: "./dist/iRingo.Maps.dev.sgmodule",
+			path: "./dist/iRingo.MapKit.dev.sgmodule",
 			template: "./template/surge.dev.handlebars",
 		},
 		loon: {
-			path: "./dist/iRingo.Maps.dev.plugin",
+			path: "./dist/iRingo.MapKit.dev.plugin",
 			template: "./template/loon.dev.handlebars",
 		},
 		customItems: [
 			{
-				path: "./dist/iRingo.Maps.dev.snippet",
+				path: "./dist/iRingo.MapKit.dev.snippet",
 				template: "./template/quantumultx.dev.handlebars",
 			},
 			{
-				path: "./dist/iRingo.Maps.dev.stoverride",
+				path: "./dist/iRingo.MapKit.dev.stoverride",
 				template: "./template/stash.dev.handlebars",
 			},
 		],
 		boxjsSettings: {
-			path: "./dist/iRingo.Maps.dev.boxjs.json",
+			path: "./dist/iRingo.MapKit.dev.boxjs.json",
 			scope: "@iRingo.Maps.Settings",
 		},
 	},
